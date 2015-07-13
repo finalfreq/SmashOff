b9f0cf1e343da1a9deb5c4bf8821fec0b2a4c981
adam mcfadden & james robert williams <student@epicodus.com>
2015-07-13 14:11:51 -0700

    add front page for duels, paperclip working with duels, render form on index page using ajax

--- app/controllers/duels_controller.rb
+++ app/controllers/duels_controller.rb
@@ -1,11 +1,26 @@
 class DuelsController < ApplicationController
-  load_and_authorize_resource
+  load_and_authorize_resource param_method: :duels_params
   def new
+    
   end
 
   def show
   end
 
   def index
+  end
+
+  def create
+    if @duel.save
+      redirect_to duel_path(@duel)
+    else
+      redirect_to :back
+    end
+  end
+
+  private def duels_params
+    params.require(:duel).permit(:name, :character_one_name, :character_two_name, :character_one_body,
+     :character_two_body, :character_one_avatar, :character_two_avatar)
+
   end
 end
--- app/models/duel.rb
+++ app/models/duel.rb
@@ -4,11 +4,19 @@ class Duel < ActiveRecord::Base
   belongs_to :user
   validates_presence_of :name, :character_two_name, :character_two_body, :character_one_name, :character_one_body
 
-  has_attached_file :character_one_avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "missing.png"
-  has_attached_file :character_two_avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "missing.png"
+  has_attached_file :character_one_avatar, :styles => { :medium => "200x200>", :thumb => "100x100>" }, :default_url => "missing.png"
+  has_attached_file :character_two_avatar, :styles => { :medium => "200x200>", :thumb => "100x100>" }, :default_url => "missing.png"
 
   validates_attachment_content_type :character_one_avatar, :content_type => /\Aimage\/.*\Z/
   validates_attachment_content_type :character_two_avatar, :content_type => /\Aimage\/.*\Z/
 
+  #can have no argument for the native image size, or one argument determining the size.
+  def character_one_image(*args)
+    character_one_avatar.url(*args)
+  end
+
+  def character_two_image(*args)
+    character_two_avatar.url(*args)
+  end
 
 end
